2f38b8754f3d03408694ebc743fde8e3d7921fe6
steven-situmorang <104076144+steven-situmorang@users.noreply.github.com>
2023-10-18 23:07:34 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,32 +1,79 @@
-<html>
-    <head>
-    <link href="asset/css/bootstrap.min.css" rel="stylesheet">
-    <title>form login</title>
-</head>
-<body>
-<div class="container col-xl-10 col-xxl-8 px-4 py-5">
-    <div class="row align-items-center g-lg-5 py-5">
-      <div class="col-lg-7 text-center text-lg-start">
-        <h1 class="display-4 fw-bold lh-1 mb-3">selmat datang di buku tamu</h1>
-        <p class="col-lg-10 fs-4">selamat datang di aplikasi buku tamu berbasis web.kami menyimpan data berupa data tamu yg hendak berkunjung.
-            jika anda sudah punya akun silahkan isi username dan password dan login
-        </p>
-      </div>
-      <div class="col-md-10 mx-auto col-lg-5">
-        <form class="p-4 p-md-5 border rounded-3 bg-light" action="proses_login.php" method="post">
-          <div class="form-floating mb-3">
-            <input type="text" class="form-control" id="username" placeholder="silahkan isi username anda" name="username">
-            <label for="username">username</label>
-          </div>
-          <div class="form-floating mb-3">
-            <input type="password" class="form-control" id="password" placeholder="Password" name="password">
-            <label for="floatingPassword">Password</label>
-          </div>
-          <button class="btn btn-lg btn-success" type="submit">Masuk</button>
-          <a href="daftar.php" class="btn btn-lg btn-primary" type="button">daftar</a>
-          <hr class="my-4">
-          <small class="text-muted">&copy copyright steven2022</small>
-        </form>
-      </div>
-    </div>
-  </div>
+<?php include 'header.php'; ?>
+
+<div class="container">
+
+	<div class="row">
+		<div class="col-lg-4 mx-auto">
+
+			<div class="pt-4 pb-3">
+
+				<center>
+					<a href="index.php"><img src="gambar/sistem/logo4.png" style="width:auto; height: 150px;"></a>
+				</center>
+
+				<br>
+				<center><h5>Login</h5></center>
+
+				<?php 
+				if(isset($_GET['alert'])){
+					if($_GET['alert'] == "registered"){
+						?>
+						<div class="alert alert-success text-center">
+							<span class="font-weight-bold">Anda berhasil mendaftar.</span>
+							<br>
+							<small class="font-weight-light">Selanjutnya silahkan login.</small>
+						</div>
+						<?php
+					}elseif($_GET['alert'] == "logout"){
+						?>
+						<div class="alert alert-success text-center">
+							<span class="font-weight-bold">Anda telah logout.</span>
+						</div>
+						<?php
+					}elseif($_GET['alert'] == "login-dulu"){
+						?>
+						<div class="alert alert-warning text-center font-weight-bold">Silahkan login untuk melanjutkan.</div>
+						<?php
+					}elseif($_GET['alert'] == "gagal"){
+						?>
+						<div class="alert alert-danger text-center">
+							<span class="font-weight-bold">Login gagal !</span>
+							<br>
+							<small class="font-weight-light">Email & password tidak sesuai.</small>
+						</div>
+						<?php
+					}
+				}
+				?>
+
+				<form action="login_aksi.php" method="post">
+
+					<div class="form-group">
+						<label>Email</label>
+						<input type="email" name="email" class="form-control" required='required' autocomplete="off" placeholder="Masukkan email ..">
+					</div>
+
+					<div class="form-group">
+						<label>Password</label>
+						<input type="password" name="password" class="form-control" required='required' placeholder="Masukkan password ..">
+					</div>
+
+					<input type="submit" class="btn btn-primary btn-block mt-4" value="LOGIN">
+
+					<p class="text-center mt-3">
+						Belum punya akun? 
+						<br>
+						<small><a href="daftar.php">Daftar Sekarang</a></small>
+					</p>
+
+				</form>
+
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
+<?php include 'footer.php'; ?>
